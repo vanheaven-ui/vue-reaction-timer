c390798dee44bd99ff1ca1062dc69a2525ef0bcc
vanheaven <vanheaven6@gmail.com>
2021-08-30 22:19:01 -0700

    Add interactivity for clicking block and track reaction time

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,39 @@
 <template>
   <h1>{{ title }} ⏱️</h1>
   <button @click="start">Play</button>
-  <Block v-if="isPlaying" :delay="delay"/>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <p v-show="showScore">Reaction Time: {{ score }} ms</p>
 </template>
 
 <script>
-import Block from './components/Block.vue'
+import Block from "./components/Block.vue";
 
 export default {
-  name: 'App',
-  components: {Block},
+  name: "App",
+  components: { Block },
   data() {
     return {
-      title: 'Vanheaven Reaction Timer',
+      title: "Vanheaven Reaction Timer",
       isPlaying: false,
       delay: null,
-    }
+      score: null,
+      showScore: false,
+    };
   },
   methods: {
     start() {
-      this.isPlaying = true
-      this.delay = 2000 + Math.random() * 5000
-    }
-  }
-}
+      this.isPlaying = true;
+      this.delay = 2000 + Math.random() * 5000;
+      this.showScore = false;
+    },
+
+    endGame(reactionTime) {
+      this.isPlaying = false;
+      this.score = reactionTime;
+      this.showScore = true;
+    },
+  },
+};
 </script>
 
 <style>
@@ -36,7 +46,7 @@ export default {
   margin-top: 60px;
 }
 
-button{
+button {
   cursor: pointer;
 }
 </style>
--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,52 +1,56 @@
 <template>
-  <div class="block" v-show="showBlock" >
+  <div class="block" v-show="showBlock" @click="stopTimer">
     <h2>Click Me!</h2>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Block',
-  props: ['delay'],
+  name: "Block",
+  props: ["delay"],
   data() {
     return {
       showBlock: false,
       timer: null,
       reactionTime: 0,
-    }
+    };
   },
 
   mounted() {
-    this.startTimer()
+    setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer();
+    }, this.delay);
   },
 
   methods: {
     startTimer() {
-      console.log(this.delay)
-      setTimeout(() => {
-        this.showBlock = true
-      }, this.delay)
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
     },
 
     stopTimer() {
-
-    }
+      clearInterval(this.timer);
+      this.$emit("end", this.reactionTime);
+      console.log(this.reactionTime);
+    },
   },
-}
+};
 </script>
 
 <style>
-  .block {
-    width: 400px;
-    cursor: pointer;
-    background-color: #0faf87;
-    color: white;
-    margin: 100px auto;
-    padding: 100px 0;
-    border-radius: 20px;
-  }
+.block {
+  width: 400px;
+  cursor: pointer;
+  background-color: #0faf87;
+  color: white;
+  margin: 100px auto;
+  padding: 100px 0;
+  border-radius: 20px;
+}
 
-  .block:hover {
-    opacity: 0.8;
-  }
+.block:hover {
+  opacity: 0.8;
+}
 </style>
