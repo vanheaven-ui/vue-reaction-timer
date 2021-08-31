738e0fd5ead7b2433c993a418ee0d88f698b1627
vanheaven <vanheaven6@gmail.com>
2021-08-30 21:47:06 -0700

    Add starter button styles

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -11,5 +11,17 @@ export default {
 </script>
 
 <style>
+  .block {
+    width: 400px;
+    cursor: pointer;
+    background-color: #0faf87;
+    color: white;
+    margin: 100px auto;
+    padding: 100px 0;
+    border-radius: 20px;
+  }
 
+  .block:hover {
+    opacity: 0.8;
+  }
 </style>
